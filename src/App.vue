10f69535a30f5614de6d016151c81966884bbe1c
Yasin ATEŞ <yasinatesim@gmail.com>
2021-08-14 21:18:42 +0300

    feat: add variables feature

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,67 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div v-if="step == 1">
+    <textarea v-model="template" cols="30" rows="10"></textarea>
+    <br /><br />
+    <button @click="handleSubmit">Submit</button>
+  </div>
+
+<div v-if="step == 2" >
+  <div v-for="variable in variables">
+    <label>
+      <strong>{{ variable }}</strong>:
+      <input type="text" :name="variable" v-model="variables[variable]" />
+      {{ variables[variable] }}
+    </label>
+    <br>
+    <br>
+  </div>
+    <button @click="handleGetTemplate">Get Tamplate</button>
+</div>
+
+  <div v-if="step == 3">
+    <textarea cols="70" rows="70" readonly>{{ result }}</textarea>
+  </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      template: "",
+      variables: "",
+      arrays: "",
+      result: "",
+      step: 1,
+    };
+  },
 
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/master/active-rfcs/0040-script-setup.md
+  methods: {
+    handleSubmit() {
+      this.variables = [
+        ...new Set(
+          this.template
+            .match(/<%=\s([A-Z_]+)+\s=%>/g)
+            .map((item) => item.replace(/[\W]+/g, "").trim())
+        ),
+      ];
+      this.step++;
+    },
+    handleGetTemplate() {
+      const variableKeys = Object.keys(this.variables);
+
+      const variablesRegexStr = variableKeys
+        .map((item) => {
+          return `<%=\\s(${item})\\s=%>`;
+        })
+        .join("|");
+
+      const variablesRegex = new RegExp(variablesRegexStr, "g");
+      this.result = this.template.replace(variablesRegex, (matched) => {
+        return this.variables[matched.replace(/<%=|=%>/g, "").trim()];
+      });
+
+      this.step++;
+    },
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
