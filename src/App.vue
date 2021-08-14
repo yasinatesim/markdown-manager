8ed566a6a42f0d08938428d58e67de1cc4eb7985
Yasin ATEŞ <yasinatesim@gmail.com>
2021-08-14 21:19:15 +0300

    chore: reformatthe code

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,19 @@
     <button @click="handleSubmit">Submit</button>
   </div>
 
-<div v-if="step == 2" >
-  <div v-for="variable in variables">
-    <label>
-      <strong>{{ variable }}</strong>:
-      <input type="text" :name="variable" v-model="variables[variable]" />
-      {{ variables[variable] }}
-    </label>
-    <br>
-    <br>
-  </div>
+  <div v-if="step == 2">
+    <div v-for="variable in variables">
+      <label>
+        <strong>{{ variable }}</strong
+        >:
+        <input type="text" :name="variable" v-model="variables[variable]" />
+        {{ variables[variable] }}
+      </label>
+      <br />
+      <br />
+    </div>
     <button @click="handleGetTemplate">Get Tamplate</button>
-</div>
+  </div>
 
   <div v-if="step == 3">
     <textarea cols="70" rows="70" readonly>{{ result }}</textarea>
